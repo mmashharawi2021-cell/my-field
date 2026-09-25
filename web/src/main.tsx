6b934ac03d17d9906319187155f94a91d5540526
mmashharawi2021-cell <mmashharawi2021@gmail.com>
2026-09-25 18:06:47 +0300

    refactor: simplify web/src/main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,27 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { HashRouter } from 'react-router-dom'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './styles/global.css'
-import App from './App'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 15_000,
-      refetchOnWindowFocus: false,
-      retry: 1,
-    },
-  },
-})
+import App from './app/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 )
