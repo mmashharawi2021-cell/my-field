fabfc44f8f4456dea9a77284a9faa0c36329bd21
mmashharawi2021-cell <mmashharawi2021@gmail.com>
2026-09-25 23:44:32 +0300

    phase03: update web/src/app/App.tsx

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,15 +1,12 @@
 import { HashRouter } from 'react-router-dom'
 import { AppProviders } from './AppProviders'
 import { AppRoutes } from './AppRoutes'
-import { AppShell } from '../components/layout/AppShell'
 
 export default function App() {
   return (
     <AppProviders>
       <HashRouter>
-        <AppShell>
-          <AppRoutes />
-        </AppShell>
+        <AppRoutes />
       </HashRouter>
     </AppProviders>
   )
