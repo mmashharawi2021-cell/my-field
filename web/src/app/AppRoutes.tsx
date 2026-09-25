8cdd7ce5d1003a7921730dbd41efc85ef29ce778
mmashharawi2021-cell <mmashharawi2021@gmail.com>
2026-09-25 23:44:34 +0300

    phase03: update web/src/app/AppRoutes.tsx

--- web/src/app/AppRoutes.tsx
+++ web/src/app/AppRoutes.tsx
@@ -1,14 +1,24 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import DashboardPage from '../pages/DashboardPage'
+import LoginPage from '../pages/LoginPage'
 import MapPage from '../pages/MapPage'
 import ProjectsPage from '../pages/ProjectsPage'
+import { ProtectedLayout } from './ProtectedLayout'
+import { ProtectedRoute } from './ProtectedRoute'
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<DashboardPage />} />
-      <Route path="/map" element={<MapPage />} />
-      <Route path="/projects" element={<ProjectsPage />} />
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route element={<ProtectedLayout />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Route>
+      </Route>
+
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
