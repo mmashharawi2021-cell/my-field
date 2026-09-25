75502bad254552f6a619d182a5dc976f8eb48215
mmashharawi2021-cell <mmashharawi2021@gmail.com>
2026-09-25 23:33:56 +0300

    phase02: update server/app/models/__init__.py

---
+++ server/app/models/__init__.py
@@ -0,0 +1,12 @@
+from app.models.auth import Role, User
+from app.models.core import ChangeLog, Feature, FeatureVersion, Layer, Project
+
+__all__ = [
+    "Role",
+    "User",
+    "Project",
+    "Layer",
+    "Feature",
+    "FeatureVersion",
+    "ChangeLog",
+]
